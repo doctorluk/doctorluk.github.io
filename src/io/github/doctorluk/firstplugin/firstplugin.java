8a7a439dcb831d075cb791785e4bd0db1c55ebb3
Lukas Monka <lukgit@gmail.com>
2013-12-29 23:11:08 +0100

    Added onCommand default function Added test logging message when initializing the plugin

--- src/io/github/doctorluk/firstplugin/firstplugin.java
+++ src/io/github/doctorluk/firstplugin/firstplugin.java
@@ -1,7 +1,27 @@
 package io.github.doctorluk.firstplugin;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class firstplugin extends JavaPlugin {
-
+	
+	@Override
+    public void onEnable(){
+		getLogger().info("onEnable has been invoked!");
+    }
+ 
+    @Override
+    public void onDisable() {
+        // TODO Insert logic to be performed when the plugin is disabled
+    }
+    
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+    	if(cmd.getName().equalsIgnoreCase("killradius")){
+    		return true;
+    	}
+    	return false; 
+    }
+    
 }
