27590d268da7d07c94bf6cb5347142629a41b79b
Lukas Monka <lukgit@gmail.com>
2013-12-30 00:56:32 +0100

    Added function to 1-argument use Added distinction between animal types when specifying one

--- src/io/github/doctorluk/firstplugin/firstplugin.java
+++ src/io/github/doctorluk/firstplugin/firstplugin.java
@@ -6,6 +6,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Animals;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -17,6 +18,26 @@ public class firstplugin extends JavaPlugin {
  
     @Override
     public void onDisable() {
+    }
+    
+    private boolean isAnimal(String animal){
+    	if(animal.equals( "CHICKEN" ))
+    		return true;
+    	if(animal.equals( "COW" ))
+    		return true;
+    	if(animal.equals( "HORSE" ))
+    		return true;
+    	if(animal.equals( "MUSHROOM_COW" ))
+    		return true;
+    	if(animal.equals( "OCELOT" ))
+    		return true;
+    	if(animal.equals( "PIG" ))
+    		return true;
+    	if(animal.equals( "SHEEP" ))
+    		return true;
+    	if(animal.equals( "WOLF" ))
+    		return true;
+    	return false;
     }
     
     /*
@@ -32,20 +53,40 @@ public class firstplugin extends JavaPlugin {
     			if(args.length == 0) // No arguments specified
     				return false;
     			
-    			if(args.length == 1 && args[0].matches("\\d+") ) // Arguments are radius only
-    				return false;
-    			
-    			if(args.length == 2 && args[1].matches("\\d+") ){ // Arguments contain animal name + radius
+    			if(args.length == 1 && args[0].matches("\\d+") ){ // Arguments are radius only
     				Player player = (Player)sender;
-    				double radius = Integer.getInteger(args[1])/2;
+    				
+    				double radius = Double.parseDouble(args[0])/2;
     				List <Entity> entities = player.getNearbyEntities(radius, radius, radius);
     				int count = 0;
     				for(Entity ent:entities){
-    					if(ent instanceof Animals)
-    						count++;
+						count++;
+						Animals ani = (Animals)ent;
+						ani.remove();
     				}
-    				getLogger().info(count+"");
-    				return false;
+    				getServer().broadcastMessage("Killed " + count + " animals.");
+    				return true;
+    			}
+    			
+    			if(args.length == 2 && args[1].matches("\\d+") ){ // Arguments contain animal name + radius
+    				Player player = (Player)sender;
+    				
+    				String animal = args[0].toUpperCase();
+    				if(!isAnimal(animal))
+    					return false;
+    				
+    				double radius = Double.parseDouble(args[1])/2;
+    				List <Entity> entities = player.getNearbyEntities(radius, radius, radius);
+    				int count = 0;
+    				for(Entity ent:entities){
+    					if( ent.getType() == EntityType.valueOf(animal) ){
+    						count++;
+    						Animals ani = (Animals)ent;
+    						ani.remove();
+    					}
+    				}
+    				getServer().broadcastMessage("Killed " + count + " of type " + args[0]);
+    				return true;
     			}
     			
     			if(args.length > 2)
