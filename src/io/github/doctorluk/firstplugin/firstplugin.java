72041dc6c2cdd2d133008073bb153b3d24f57de1
Lukas Monka <lukgit@gmail.com>
2013-12-31 02:52:39 +0100

    onChestHit added Will send a message to the player when there is an apple in the chest

--- src/io/github/doctorluk/firstplugin/firstplugin.java
+++ src/io/github/doctorluk/firstplugin/firstplugin.java
@@ -2,18 +2,25 @@ package io.github.doctorluk.firstplugin;
 
 import java.util.List;
 
+import org.bukkit.Material;
+import org.bukkit.block.Chest;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Animals;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class firstplugin extends JavaPlugin {
+public class firstplugin extends JavaPlugin implements Listener {
 	
 	@Override
     public void onEnable(){
+		getServer().getPluginManager().registerEvents(this, this);
     }
  
     @Override
@@ -38,6 +45,16 @@ public class firstplugin extends JavaPlugin {
     	if(animal.equals( "WOLF" ))
     		return true;
     	return false;
+    }
+    
+    @EventHandler
+    public void onChestHit(PlayerInteractEvent event){
+    	if( event.getAction() == Action.LEFT_CLICK_BLOCK && event.getClickedBlock().getType() == Material.CHEST ) {
+    		Chest chest = (Chest)event.getClickedBlock().getState();
+    		Player player = event.getPlayer();
+    		if( chest.getInventory().contains(Material.APPLE) )
+    			player.sendMessage("There is an APPLE in the chest!");
+		}
     }
     
     /*
